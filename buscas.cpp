ba93665326d4ba247b71d0a3fd6a95ecbb75e8c9
badboytux <marcielmanoel15@gmail.com>
2015-08-11 00:08:44 -0300

    11/08- 00:08

--- buscas.cpp
+++ buscas.cpp
@@ -1,38 +1,45 @@
 #include <iostream>
 
-int buscaSequencialI(int num,int *p,int tamanho){
+int buscaSequencialI(int num,int *v,int tamanho){
 	int i;
 	for(i=0;i<tamanho;i++){
-		if(*(p+i)==num) return i;
+		if(*(v+i)==num) return i;
 	}
 	return -1;
 }
 
-int buscaSequencialR(int num, int *p,int tamanho){
+int buscaSequencialR(int num, int *v,int tamanho){
 	if(tamanho<=0) return -1;
-	else if(*(p+(tamanho-1))==num) return (tamanho-1);
+	else if(*(v+(tamanho-1))==num) return (tamanho-1);
 	
-	return buscaSequencialR(num,p,tamanho-1);
+	return buscaSequencialR(num,v,tamanho-1);
 }
 
-int buscaBinariaI(int num,int *p, int tamanho){
+int buscaBinariaI(int num,int *v, int tamanho){
 	int e,m,d;
 	e=-1; d=tamanho;
 	while(e<d-1){
 		m=(e+d)/2;
-		if(*(p+m)<num) e=m;
-		else if(*(p+m)>num) d=m;
+		if(*(v+m)<num) e=m;
+		else if(*(v+m)>num) d=m;
 		else return m;
 	}
 	return -1;
 }
-int buscaBinariaR(int num,int *p, int tamanho){
-	if(tamanho<=0) return -1;
-	
-	return 0;
+int buscaBinariaR2(int num,int *v, int e,int d){
+	if(e>=d-1) return -1;
+	else{
+		int m=(e+d)/2;
+		if(*(v+m)<num) return buscaBinariaR2(num,v,m,d);
+		else if(*(v+m)<num) return buscaBinariaR2(num,v,e,m);
+		else return m;
+	}
+}
+int buscaBinariaR(int num,int *v, int tamanho){
+	return buscaBinariaR2(num,v,-1,tamanho);
 }
 int main(){
 	int a[9]={12, 15, 16, 20, 22, 28, 40, 60, 90};
-	std::cout<< buscaBinariaI(90,a,9)<<std::endl;
+	std::cout<< buscaBinariaR(12,a,9)<<std::endl;
 	return 0;
 }
