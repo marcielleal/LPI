d8d84da3c6d7040f2ddc1e10b6c6edf897bff6da
Marciel Manoel Leal <badboytux@imd.(none)>
2015-08-25 21:45:10 -0300

    Exercise 2

--- L3E2.cpp
+++ L3E2.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
-int compara(void *a,void *b){
+int comparaCres(void *a,void *b){
 	int m=*((int*)a);
 	int n=*((int*)b);
 	if(m<n) return 1;
 	return 0;
 }
+int comparaDesc(void *a,void *b){
+	int m=*((int*)a);
+	int n=*((int*)b);
+	if(m>n) return 1;
+	return 0;
+}
 void selection_sort(int compara(void*,void*),int *v, int tam){
 	int i, j, min, aux;
-	for (i = 0; i<(tam-1); i++) {
+	for (i = 0; i<=(tam); i++) {
 		min = i;
 		for (j = (i+1); j <tam; j++) {
 			if(compara((v+j),(v+min)))
@@ -21,10 +27,14 @@ void selection_sort(int compara(void*,void*),int *v, int tam){
 	}
 }
 int main(void){
-	int a[]={12,14,7,9,10,-14},i;
-	selection_sort(compara,a,6);
+	int a[]={12,15,7,9,10,-14},i;
+	selection_sort(comparaCres,a,6);
 	for(i=0;i<5;i++){
-		std::cout<<a[i]<<std::endl;
+		std::cout<<a[i]<<" ";
+	}std::cout<<std::endl;
+	selection_sort(comparaDesc,a,6);
+	for(i=0;i<5;i++){
+		std::cout<<a[i]<<" ";
 	}
 	return 0;
 }
