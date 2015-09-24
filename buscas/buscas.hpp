61e58171e42e386aec50148a0a43fc153d28c704
Aluno <aluno@imd.(none)>
2015-09-24 17:51:52 -0300

    OP

---
+++ buscas/buscas.hpp
@@ -0,0 +1,9 @@
+#ifndef BUSCAS_H_INCLUDED
+#define BUSCAS_H_INCLUDED
+
+int buscaSequencialI(int num,int *v,int tamanho);
+int buscaSequencialR(int num, int *v,int tamanho);
+int buscaBinariaI(int num,int *v, int tamanho);
+int buscaBinariaR2(int num,int *v, int e,int d);
+int buscaBinariaR(int num,int *v, int tamanho);
+#endif // BUSCAS_H_INCLUDED
