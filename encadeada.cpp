1cd8d940c5959f7e5adec82ed96a73e7a6fdef45
Marciel Manoel Leal <badboytux@imd.(none)>
2015-09-25 15:23:47 -0300

    MOP

--- encadeada.cpp
+++ encadeada.cpp
@@ -6,7 +6,7 @@ typedef struct Encadeada{
 }Encadeada;
 
 Encadeada *create(void){
-	return new NULL;
+	return NULL;
 }
 
 Encadeada *add1(Encadeada *lista, int data){
@@ -17,44 +17,73 @@ Encadeada *add1(Encadeada *lista, int data){
 }
 
 Encadeada *add(Encadeada *lista,int data){
-	Encadeada *inicio=lista;
-	while(lista->next) lista=lista->next;
 	Encadeada *novo=new Encadeada;
 	novo->data=data;
 	novo->next=NULL;
-	lista->next=novo;
-	return inicio;
+	
+	if(lista){
+		Encadeada *inicio=lista;
+		while(lista->next) lista=lista->next;
+		lista->next=novo;	
+		return inicio;
+	}return novo;
 }
 
-Encadeada *busca(Encadeada *lista,int v){
+Encadeada *busca(Encadeada *inicio,int v){
+	Encadeada *lista=inicio;
 	while((lista->data!=v)&&lista->next) lista=lista->next;
-	if (lista->data==v) return lista;
+	if(lista->data==v) return lista;
 	return NULL;
 }
-/* Pensar
-Encadeada *apaga(Encadeada *lista,int v){
-	while((lista->data!=v)&&lista->next) lista++;
-	if(lista->data==v){ 
-		if(lista->next){
-			(lista-1)->next=lista+1;
+
+Encadeada *apaga(Encadeada *inicio,int v){
+	Encadeada *lista=inicio;
+	lista=busca(lista,v);
+	if(lista){
+		if(lista==inicio){
+			inicio=inicio->next;
 			delete lista;
-			return lista;
 		}else{
-			lista--;
-			lista->next=NULL;
-			return lista;
+			Encadeada *novo=inicio;
+			while(novo->next!=lista) novo=novo->next;
+			novo->next=lista->next;
+			delete lista;
+		}return inicio;
 	}return NULL;
 }
-*/
+
 bool igual(Encadeada *lista1,Encadeada *lista2){
 	while((lista1->next&&lista2->next)&&(lista1->data==lista2->data)){
 		lista1=lista1->next;
 		lista2=lista2->next;
-	}if((!lista1->next&&!lista2->next)&&(lista1->data==lista2->data) return 1;
+	}if((!lista1->next&&!lista2->next)&&(lista1->data==lista2->data)) return 1;
 	return 0;
 }
-
+void imprime(Encadeada *lista){
+	Encadeada *pont=lista;
+	for(;pont;pont=pont->next)
+		std::cout<<pont->data<<" ";
+	std::cout<<std::endl;
+}
 int main(void){
-	
+	Encadeada *pont=create();
+		pont=add(pont,24);
+	imprime(pont);
+	pont=add(pont,36);
+	imprime(pont);
+	pont=add1(pont,12);
+	imprime(pont);
+	pont=add1(pont,15);
+	imprime(pont);
+	pont=add1(pont,7);
+	imprime(pont);
+	pont=apaga(pont,24);
+	imprime(pont);
+	pont=apaga(pont,36);
+	imprime(pont);
+	pont=apaga(pont,7);
+	imprime(pont);
+	pont=apaga(pont,0);
+	imprime(pont);
 	return 0;
 }
